5fbedaeb08d65704b66d4a069c534ca40c51fc5c
sima9z <159206331+sima9z@users.noreply.github.com>
2024-05-24 13:51:03 +0900

    API検証

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -1,5 +1,25 @@
 class SignsController < ApplicationController
 
   def index
+    require 'faraday' # Faradayを使用する場合
+
+    # Bearer Tokenを設定
+    token = ENV['BEARER_TOKEN']
+
+    # Faradayのインスタンスを作成
+    conn = Faraday.new(url: 'https://app.livlog.xyz/hoshimiru/constellation')
+
+    # リクエストヘッダーに認証情報を含める
+    conn.headers['Authorization'] = "Bearer #{token}"
+    conn.headers['Accept'] = 'application/json'
+
+    # APIエンドポイントにリクエストを送信
+    response = conn.get do |req|
+      req.url '', { lat: '35.6581', lng: '139.7414', date: '2020-01-15', hour: '20', min: '00', id: '1', disp: 'on' }
+    end
+
+    # レスポンスを解析
+    @data = JSON.parse(response.body)
   end
+
 end
