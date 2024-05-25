21bd92ec31f9e2680f4737af327d56619a94d194
sima9z <159206331+sima9z@users.noreply.github.com>
2024-05-25 21:40:19 +0900

    TOPpage追加

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,25 @@
 class TopsController < ApplicationController
 
   def top
+    require 'faraday'
+    require 'json'
 
+    token = ENV['BEARER_TOKEN']
+
+    conn = Faraday.new(url: 'https://app.livlog.xyz/hoshimiru/constellation') do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+      faraday.headers['Authorization'] = "Bearer #{token}"
+    end
+
+    response = conn.get do |req|
+      req.url '', { lat: 35.6581, lng: 139.7414, date: '2024-05-25', hour: '20', min: '00' }
+    end
+
+    @data = JSON.parse(response.body)
+
+ 
   end
 
 end
