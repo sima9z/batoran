75572a4c46dbea765e48d5da82bd05c44a1060f6
sima9z <159206331+sima9z@users.noreply.github.com>
2024-05-27 18:54:43 +0900

    showページ

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,9 +1,8 @@
 class PlanetsController < ApplicationController
+  require 'faraday'
+  require 'json'
 
   def index
-    require 'faraday'
-    require 'json'
-
     # 現在の日時を取得
     current_time = Time.now
 
@@ -27,5 +26,25 @@ class PlanetsController < ApplicationController
   end
 
   def show
+        # 現在の日時を取得
+        current_time = Time.now
+
+        # Bearer 
+        token = ENV['BEARER_TOKEN']
+    
+        # 惑星の情報を取得
+        planets_conn = Faraday.new(url: 'https://app.livlog.xyz/hoshimiru/planet') do |faraday|
+          faraday.request  :url_encoded
+          faraday.response :logger
+          faraday.adapter  Faraday.default_adapter
+          faraday.headers['Authorization'] = "Bearer #{token}"
+          faraday.headers['Accept'] = 'application/json'
+        end
+    
+        planets_response = planets_conn.get do |req|
+          req.url '', { lat: 35.6581, lng: 139.7414, date: current_time.strftime('%Y-%m-%d'), hour: current_time.strftime('%H'), min: current_time.strftime('%M'), id: params[:id] }
+        end
+    
+        @planets_data = JSON.parse(planets_response.body)    
   end
 end
--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -1,9 +1,8 @@
 class SignsController < ApplicationController
+  require 'faraday' # Faradayを使用する場合
+  require 'json'
 
   def index
-    require 'faraday' # Faradayを使用する場合
-    require 'json'
-
     # Bearer 
     token = ENV['BEARER_TOKEN']
 
@@ -26,6 +25,25 @@ class SignsController < ApplicationController
   end
 
   def show
+    # Bearer 
+    token = ENV['BEARER_TOKEN']
+
+    current_time = Time.now
+
+    # Faradayのインスタンスを作成
+    conn = Faraday.new(url: 'https://app.livlog.xyz/hoshimiru/constellation')
+
+    # リクエストヘッダーに認証情報を含める
+    conn.headers['Authorization'] = "Bearer #{token}"
+    conn.headers['Accept'] = 'application/json'
+
+    # APIエンドポイントにリクエストを送信
+    response = conn.get do |req|
+      req.url '', { lat: '35.6895', lng: '139.6917', date: current_time.strftime('%Y-%m-%d'), hour: current_time.strftime('%H'), min: current_time.strftime('%M'), id: params[:id], disp:"on" }
+    end
+
+    # レスポンスを解析
+    @data = JSON.parse(response.body)
   end
 
 end
