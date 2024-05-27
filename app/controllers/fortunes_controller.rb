535aaa208467063d65d220ec81eed1a3129f1ac5
sima9z <159206331+sima9z@users.noreply.github.com>
2024-05-27 20:22:26 +0900

    占いAPI

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,7 +1,17 @@
 class FortunesController < ApplicationController
+  require 'faraday'
+  require 'json'
 
   def index
+    data = Time.now.strftime("%Y/%m/%d")
 
+    conn = Faraday.new(url: "http://api.jugemkey.jp/api/horoscope/free/#{data}")
+
+    response = conn.get
+
+    @data = JSON.parse(response.body)
+
+    puts @data
   end
 
 end
