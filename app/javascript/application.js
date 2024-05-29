bca8cbddba01ce76d1d4d47b2d0df5c48e1d8314
sima9z <159206331+sima9z@users.noreply.github.com>
2024-05-29 15:50:48 +0900

    javascriptリファクタリング

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $.ajax({
+      url: '/update_location',
+      type: 'POST',
+      data: { 
+        latitude: position.coords.latitude, 
+        longitude: position.coords.longitude 
+      },
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))}
+    });
+  });
